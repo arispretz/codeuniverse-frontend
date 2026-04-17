638cf2c310554eab8436706809584e0bf03ec026
arispretz <arispretz@gmail.com>
2026-04-17 20:47:59 -0300

    fix: width

--- src/pages/CollaborativeCodeEditor.jsx
+++ src/pages/CollaborativeCodeEditor.jsx
@@ -95,7 +95,7 @@ const CollaborativeCodeEditor = () => {
   return (
     <Box sx={{ display: 'flex', width: '100%', minHeight: '100vh', overflow: 'hidden' }}>
       {/* 📂 File Tree */}
-      <Box sx={{ width: 150, borderRight: '1px solid #444', overflowY: 'auto' }}>
+      <Box sx={{ width: 150, borderRight: '1px solid #444', overflowY: 'auto', ml: '200px' }}>
         <FileTree
           projectId={projectId}
           onOpenFile={handleOpenFile}
