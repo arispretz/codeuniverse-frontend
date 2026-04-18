efc243a02a86264c040d6b8e9516fdd4a55e2ab6
arispretz <arispretz@gmail.com>
2026-04-18 17:43:55 -0300

    update: layour

--- src/pages/CollaborativeCodeEditor.jsx
+++ src/pages/CollaborativeCodeEditor.jsx
@@ -93,7 +93,7 @@ const CollaborativeCodeEditor = () => {
   };
 
   return (
-    <Box sx={{ display: 'flex', width: '90%', minHeight: '100vh', overflow: 'hidden' }}>
+    <Box sx={{ display: 'flex', width: '95%', minHeight: '100vh', overflow: 'hidden' }}>
       {/* 📂 File Tree */}
       <Box sx={{ width: 180, borderRight: '1px solid #444', overflowY: 'auto' }}>
         <FileTree
